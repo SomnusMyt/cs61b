b189a413612ab17fea7c7a964cd358ceb9ccdd5d
dcp <964386385@qq.com>
2020-05-22 20:22:14 +0800

    proj1a-0

---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,102 @@
+public class LinkedListDeque<T> {
+    public Node sentinel;
+    public int size;
+
+    public class Node{
+        public T item;
+        public Node prev;
+        public Node next;
+
+        public Node(Node p, T i, Node n){
+            prev = p;
+            item = i;
+            next = n;
+        }
+    }
+
+    public LinkedListDeque(){
+        sentinel = new Node(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+        size = 0;
+    }
+
+    public LinkedListDeque(LinkedListDeque other){
+        sentinel = new Node(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+        size = 0;
+
+        for (int i = 0; i < other.size(); i++){
+            addLast((T) other.get(i));
+        }
+    }
+
+    public void addFirst(T item){
+        size++;
+        Node t = new Node(sentinel, item, sentinel.next);
+        sentinel.next.prev = t;
+        sentinel.next = t;
+    }
+
+    public void addLast(T item){
+        size++;
+        Node t = new Node(sentinel.prev, item, sentinel);
+        sentinel.prev.next = t;
+        sentinel.prev = t;
+    }
+
+    public boolean isEmpty(){
+        if(sentinel.next == sentinel)
+            return true;
+        return false;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void printDeque(){
+        Node p = sentinel.next;
+        while ( p != sentinel) {
+            System.out.print((String) p.item + " ");
+            p = p.next;
+        }
+        System.out.print("\n");
+    }
+
+    public T removeFirst(){
+        if (isEmpty())
+            return null;
+        T t = sentinel.next.item;
+        sentinel.next.next.prev = sentinel;
+        sentinel.next =  sentinel.next.next;
+        return t;
+    }
+
+    public T removeLast(){
+        if (isEmpty())
+            return null;
+        T t = sentinel.prev.item;
+        sentinel.prev.prev.next = sentinel;
+        sentinel.prev = sentinel.prev.prev;
+        return t;
+    }
+
+    public T get(int index){
+        if (index > size())
+            return null;
+        Node p = sentinel;
+        for (int i = 0; i<index; i++){
+            p = p.next;
+        }
+        return p.item;
+    }
+
+    public static void main(String[] args) {
+        LinkedListDeque<Integer> L = new LinkedListDeque<Integer>();
+        L.addLast(10);
+        L.addLast(5);
+        System.out.print(L.size());
+    }
+}
