6528812562a56f7a334f5654db11691e4ecab2e2
dcp <964386385@qq.com>
2020-05-23 14:51:36 +0800

    proj1a-2

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,42 +1,42 @@
-public class ArrayDeque<Item> {
-    private Item[] items;
+public class ArrayDeque<T> {
+    private T[] items;
     private int size;
     private int nextFirst;
     private int nextLast;
 
-    public ArrayDeque(){
-        items = (Item[]) new Object[8];
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
         size = 0;
         nextFirst = 4;
         nextLast = 5;
     }
 
-    public ArrayDeque(ArrayDeque other){
-        items = (Item[]) new Object[other.items.length];
+    public ArrayDeque(ArrayDeque other) {
+        items = (T[]) new Object[other.items.length];
         size = 0;
         nextFirst = other.nextFirst;
         nextLast = other.nextLast;
         int i = plusOne(nextFirst);
-        while( i != nextLast){
-            items[i] = (Item) other.items[i];
+        while ( i != nextLast) {
+            items[i] = (T) other.items[i];
             i = plusOne(i);
         }
 
     }
 
-    public int minusOne(int index){
+    public int minusOne(int index) {
         return (index - 1 + items.length) % items.length;
 
     }
 
-    public int plusOne(int index){
+    public int plusOne(int index) {
         return (index + 1) % items.length;
     }
 
     private void resize(int capacity) {
-        Item[] a = (Item[]) new Object[capacity];
+        T[] a = (T[]) new Object[capacity];
         int t = plusOne(nextFirst);
-        for(int i = 0; i < size; i++){
+        for (int i = 0; i < size; i++) {
             a[i] = items[t];
             t = plusOne(t);
         }
@@ -46,8 +46,8 @@ public class ArrayDeque<Item> {
     }
 
     /** Inserts X into the front of the list.*/
-    public void addFirst(Item item){
-        if(size == items.length){
+    public void addFirst(T item) {
+        if (size == items.length) {
             resize(size * 2);
         }
 
@@ -57,7 +57,7 @@ public class ArrayDeque<Item> {
     }
 
     /** Inserts X into the back of the list. */
-    public void addLast(Item x) {
+    public void addLast(T x) {
         if (size == items.length) {
             resize(size * 2);
         }
@@ -67,18 +67,18 @@ public class ArrayDeque<Item> {
         nextLast = minusOne(nextLast);
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
     /** Returns the number of items in the list. */
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public void printDeque(){
+    public void printDeque() {
         int i = nextFirst++;
-        while( i != nextLast){
+        while (i != nextLast) {
             System.out.print((String) items[i] + " ");
             i++;
         }
@@ -86,37 +86,41 @@ public class ArrayDeque<Item> {
     }
 
     /** Returns the item from the back of the list. */
-    public Item getLast() {
+    public T getLast() {
         return items[size - 1];
     }
     /** Gets the ith item in the list (0 is the front). */
-    public Item get(int i) {
+    public T get(int i) {
         return items[i];
     }
 
-    public Item removeFirst(){
-        if (size == 0)
+    public T removeFirst() {
+        if (size == 0) {
             return null;
+        }
         nextFirst = plusOne(nextFirst);
-        Item res = items[nextFirst];
+        T res = items[nextFirst];
         items[nextFirst] = null;
         size--;
-        if (items.length >= 16 && size < items.length * 0.25)
+        if (items.length >= 16 && size < items.length * 0.25) {
             resize(size / 2);
+        }
         return res;
     }
 
     /** Deletes item from back of the list and
      * returns deleted item. */
-    public Item removeLast() {
-        if (size == 0)
+    public T removeLast() {
+        if (size == 0) {
             return null;
+        }
         nextLast = minusOne(nextLast);
-        Item res = items[nextLast];
+        T res = items[nextLast];
         items[nextLast] = null;
         size--;
-        if (items.length >= 16 && size < items.length * 0.25)
+        if (items.length >= 16 && size < items.length * 0.25) {
             resize(size / 2);
+        }
         return res;
     }
     public static void main(String[] args) {
