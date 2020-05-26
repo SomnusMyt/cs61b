9f1ff23dd62e779dba98c93c3200c204a5b5bf37
dcp <964386385@qq.com>
2020-05-26 17:56:17 +0800

    proj1b-1

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,6 +1,6 @@
 public class Palindrome {
     public Deque<Character> wordToDeque(String word) {
-        Deque<Character> l = new LinkedListDeque<Character>();
+        Deque<Character> l = new ArrayDeque<Character>();
         for (int i = 0; i < word.length(); i++) {
             l.addLast(word.charAt(i));
         }
@@ -8,10 +8,20 @@ public class Palindrome {
     }
 
     public boolean isPalindrome(String word) {
-        boolean flag = true;
+        /*boolean flag = true;
         for (int i = 0; i < word.length() / 2; i++) {
             int j = word.length() - 1 - i;
             if (word.charAt(i) != word.charAt(j)) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;*/
+        Deque<Character> l = wordToDeque(word);
+        boolean flag = true;
+        for (int i = 0; i < word.length() / 2; i++) {
+            int j = word.length() - 1 - i;
+            if (l.get(i) != l.get(j)) {
                 flag = false;
                 break;
             }
--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -19,7 +19,7 @@ public class TestPalindrome {
     @Test
     public void testIsPalindrome() {
         CharacterComparator cc = new OffByN(5);
-        assertTrue(palindrome.isPalindrome("af",cc));
+        assertTrue(palindrome.isPalindrome("af", cc));
 
     }
 }
