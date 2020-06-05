027704e67bf6d8bcacd8a68973b163e0f42087c2
dcp <964386385@qq.com>
2020-06-05 08:43:54 +0800

    proj1gold-4

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -4,33 +4,64 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TestArrayDequeGold {
-
     @Test
-    public void testArratDeque2() {
-        ArrayDequeSolution<Integer> expect = new ArrayDequeSolution<>();
-        StudentArrayDeque<Integer> input = new StudentArrayDeque<>();
-        int random = StdRandom.uniform(100);
-        expect.addFirst(random);
-        input.addFirst(random);
-        assertEquals("addFirst("+random+")", expect.get(0), input.get(0));
-        System.out.println("addFirst("+random+")");
+    public void testArrayDeque() {
+        ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
+        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+        // addLast
+        for (int i=0; i<10; i++) {
+            int random = StdRandom.uniform(100);
+            ads.addLast(random);
+            sad.addLast(random);
+            int actual = ads.get(i);
+            int expected = sad.get(i);
+            assertEquals("addLast(" + random + ")",
+                    expected, actual);
+            System.out.println("addLast(" + random + ")");
+        }
 
-        random = StdRandom.uniform(100);
-        expect.addLast(random);
-        input.addLast(random);
-        assertEquals("addLast("+random+")", expect.get(1), input.get(1));
-        System.out.println("addLast("+random+")");
 
-        int actual = expect.removeFirst();
-        int expected = expect.removeFirst();
-        assertEquals("removeFirst()", actual, expected);
-        System.out.println("removeFirst()");
+        // addFirst
+        for (int i=0; i<10; i++) {
+            int random = StdRandom.uniform(100);
+            ads.addFirst(random);
+            sad.addFirst(random);
+            int actual = ads.get(i);
+            int expected = sad.get(i);
+            assertEquals("addFirst(" + random + ")",
+                    expected, actual);
+            System.out.println("addFirst(" + random + ")");
+        }
 
-        actual = expect.removeLast();
-        expected = input.removeLast();
-        assertEquals("removeLast()", actual, expected);
-        System.out.println("removeLast()");
-    }
+        // removeFirst
+        List<Integer> actualList = new ArrayList<>();
+        List<Integer> expectedList = new ArrayList<>();
+        for (int i=0; i<10; i++) {
+            actualList.add(ads.removeFirst());
+            expectedList.add(sad.removeFirst());
+            int actual = actualList.get(i);
+            int expected = expectedList.get(i);
+            assertEquals("removeFirst()",
+                    expected, actual);
+            System.out.println("removeFirst()");
+        }
+
+        // removeLast
+        actualList.clear();
+        expectedList.clear();
+        for (int i=0; i<10; i++) {
+            actualList.add(ads.removeLast());
+            expectedList.add(sad.removeLast());
+            int actual = actualList.get(i);
+            int expected = expectedList.get(i);
+            assertEquals("removeLast()",
+                    expectedList.get(i), actualList.get(i));
+            System.out.println("removeLast()");
+        }
+
+
+        }
+
 
 
 }
